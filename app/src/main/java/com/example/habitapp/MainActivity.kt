eeafd64bbc619d42af4845dbdaa9b03e96e7cb93
Felipe Romero <87445291+Pipe7993@users.noreply.github.com>
2025-12-18 18:37:21 -0300

    fix primera vista estadisticas

--- app/src/main/java/com/example/habitapp/MainActivity.kt
+++ app/src/main/java/com/example/habitapp/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        mostrarFragmento(fragmentHabitos)
+        mostrarFragmento(fragmentEstadisticas)
 
         barraNavegacion.setOnItemSelectedListener { item ->
             when (item.itemId) {
